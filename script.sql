bac7e77ca4dd52283812861653dd76274a39644f
ICWYM <103774858+ICWYM@users.noreply.github.com>
2024-03-31 21:28:26 +0700

    Update and rename scipt.sql to script.sql

---
+++ script.sql
@@ -0,0 +1,285 @@
+﻿USE [dbTech]
+GO
+/****** Object:  Table [dbo].[Accounts]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Accounts](
+	[AccountID] [int] IDENTITY(1,1) NOT NULL,
+	[Phone] [varchar](12) NULL,
+	[Email] [nvarchar](50) NULL,
+	[Password] [nvarchar](50) NULL,
+	[Salt] [nchar](10) NULL,
+	[Active] [bit] NOT NULL,
+	[FullName] [nvarchar](150) NULL,
+	[RoleID] [int] NULL,
+	[LastLogin] [datetime] NULL,
+	[CreateDate] [datetime] NULL,
+ CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED 
+(
+	[AccountID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Categories]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CatID] [int] IDENTITY(1,1) NOT NULL,
+	[CatName] [nvarchar](250) NULL,
+	[Description] [nvarchar](max) NULL,
+	[ParentID] [int] NULL,
+	[Levels] [int] NULL,
+	[Ordering] [int] NULL,
+	[Published] [bit] NOT NULL,
+	[Thumb] [nvarchar](250) NULL,
+	[Title] [nvarchar](250) NULL,
+	[Alias] [nvarchar](250) NULL,
+	[MetaDesc] [nvarchar](250) NULL,
+	[MetaKey] [nvarchar](250) NULL,
+	[Cover] [nvarchar](255) NULL,
+	[SchemaMarkup] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[CatID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[FullName] [nvarchar](255) NULL,
+	[Birthday] [datetime] NULL,
+	[Avatar] [nvarchar](255) NULL,
+	[Address] [nvarchar](255) NULL,
+	[Email] [nchar](150) NULL,
+	[Phone] [varchar](12) NULL,
+	[LocationID] [int] NULL,
+	[District] [int] NULL,
+	[Ward] [int] NULL,
+	[CreateDate] [datetime] NULL,
+	[Password] [nvarchar](50) NULL,
+	[Salt] [nchar](8) NULL,
+	[LastLogin] [datetime] NULL,
+	[Active] [bit] NOT NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Locations]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Locations](
+	[LocationID] [int] NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Parent] [int] NULL,
+	[Levels] [int] NULL,
+	[Slug] [nvarchar](100) NULL,
+	[NameWithType] [nvarchar](100) NULL,
+	[Type] [nvarchar](10) NULL,
+ CONSTRAINT [PK_Locations] PRIMARY KEY CLUSTERED 
+(
+	[LocationID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OrderDetails]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetails](
+	[OrderDetailID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderID] [int] NULL,
+	[ProductID] [int] NULL,
+	[OrderNumber] [int] NULL,
+	[Amount] [int] NULL,
+	[Discount] [int] NULL,
+	[TotalMoney] [int] NULL,
+	[CreateDate] [datetime] NULL,
+	[Price] [int] NULL,
+ CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED 
+(
+	[OrderDetailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NULL,
+	[OrderDate] [datetime] NULL,
+	[ShipDate] [datetime] NULL,
+	[TransactStatusID] [int] NOT NULL,
+	[Deleted] [bit] NOT NULL,
+	[Paid] [bit] NOT NULL,
+	[PaymentDate] [datetime] NULL,
+	[TotalMoney] [int] NOT NULL,
+	[PaymentID] [int] NULL,
+	[Note] [nvarchar](max) NULL,
+	[Address] [nvarchar](max) NULL,
+	[LocationID] [int] NULL,
+	[District] [int] NULL,
+	[Ward] [int] NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Pages]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Pages](
+	[PageID] [int] IDENTITY(1,1) NOT NULL,
+	[PageName] [nvarchar](250) NULL,
+	[Contents] [nvarchar](max) NULL,
+	[Thumb] [nvarchar](250) NULL,
+	[Published] [bit] NOT NULL,
+	[Title] [nvarchar](250) NULL,
+	[MetaDesc] [nvarchar](250) NULL,
+	[MetaKey] [nvarchar](250) NULL,
+	[Alias] [nvarchar](250) NULL,
+	[CreatedDate] [datetime] NULL,
+	[Ordering] [int] NULL,
+ CONSTRAINT [PK_Pages] PRIMARY KEY CLUSTERED 
+(
+	[PageID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Products]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [nvarchar](255) NOT NULL,
+	[ShortDesc] [nvarchar](255) NULL,
+	[Description] [nvarchar](max) NULL,
+	[CatID] [int] NOT NULL,
+	[Price] [int] NULL,
+	[Discount] [int] NULL,
+	[Thumb] [nvarchar](255) NULL,
+	[Video] [nvarchar](255) NULL,
+	[DateCreated] [datetime] NULL,
+	[DateModified] [datetime] NULL,
+	[BestSellers] [bit] NOT NULL,
+	[HomeFlag] [bit] NOT NULL,
+	[Active] [bit] NOT NULL,
+	[Tags] [nvarchar](max) NULL,
+	[Title] [nvarchar](255) NULL,
+	[Alias] [nvarchar](255) NULL,
+	[MetaDesc] [nvarchar](255) NULL,
+	[MetaKey] [nvarchar](255) NULL,
+	[UnitsInStock] [int] NOT NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Roles]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[RoleID] [int] IDENTITY(1,1) NOT NULL,
+	[RoleName] [nvarchar](50) NULL,
+	[Description] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[RoleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[TransactStatus]    Script Date: 3/31/2024 9:27:18 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TransactStatus](
+	[TransactStatusID] [int] IDENTITY(1,1) NOT NULL,
+	[Status] [nvarchar](50) NULL,
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_TransactStatus] PRIMARY KEY CLUSTERED 
+(
+	[TransactStatusID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Accounts]  WITH CHECK ADD  CONSTRAINT [FK_Accounts_Roles] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Roles] ([RoleID])
+GO
+ALTER TABLE [dbo].[Accounts] CHECK CONSTRAINT [FK_Accounts_Roles]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY([OrderID])
+REFERENCES [dbo].[Orders] ([OrderID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Orders]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Products]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_TransactStatus] FOREIGN KEY([TransactStatusID])
+REFERENCES [dbo].[TransactStatus] ([TransactStatusID])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_TransactStatus]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Categories] FOREIGN KEY([CatID])
+REFERENCES [dbo].[Categories] ([CatID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Categories]
+GO
+
+GO
+
+INSERT INTO [dbo].[Roles]
+           ([RoleName]
+           ,[Description])
+     VALUES
+           ('Admin','Admin'),('STAFF','Nhân Viên')
+GO
+USE [dbTech]
+GO
+
+INSERT INTO [dbo].[Accounts]
+           ([Phone]
+           ,[Email]
+           ,[Password]
+           ,[Salt]
+           ,[Active]
+           ,[FullName]
+           ,[RoleID]
+           ,[LastLogin]
+           ,[CreateDate])
+     VALUES
+		   ('0123456789','admin@gmail.com','123456','',1,'Nguyen',1,'','')
+GO
